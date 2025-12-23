ccff828c4a6b37858bdb727bfb5237cdf62e18c2
Akash Shrestha <akashrestha01@gmail.com>
2025-12-23 15:45:13 -0500

    Add Github Actions deploy to S3

--- main.tf
+++ main.tf
@@ -323,6 +323,7 @@ resource "aws_s3_bucket" "artifacts" {
 
 resource "aws_s3_bucket_versioning" "artifacts" {
   bucket = aws_s3_bucket.artifacts.id
+
   versioning_configuration {
     status = "Enabled"
   }
